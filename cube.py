fe7814b51e09783200083e342799d18e29960865
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-05-26 09:16:11 +0200

    clear current scene objects before reset

--- cube.py
+++ cube.py
@@ -1,8 +1,10 @@
 import bpy
  
-D = bpy.data
-C = bpy.context
+# delete all objects in the current scene
+for obj in bpy.context.scene.objects:
+    bpy.data.objects.remove(obj, do_unlink=True)
+
 
 bpy.ops.mesh.primitive_cube_add(size=2)
-bpy.context.object.location = (1, 2, 5)
+bpy.context.object.location = (1,4, 1)
 
