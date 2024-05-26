114b2cf5fa4c839b6226bd2fe634163f3892242b
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-05-26 12:50:58 +0200

    added view switch

--- grease_pencil_demo.py
+++ grease_pencil_demo.py
@@ -31,25 +31,57 @@ def create_grease_pencil_frame(layer):
         frame = layer.frames[0]
     return frame
 
+def switch_view(type):
+    if type == 'FRONT':
+        bpy.ops.view3d.view_axis(type='FRONT')
+    elif type == 'BACK':
+        bpy.ops.view3d.view_axis(type='BACK')
+    elif type == 'TOP':
+        bpy.ops.view3d.view_axis(type='TOP')
+    elif type == 'BOTTOM':
+        bpy.ops.view3d.view_axis(type='BOTTOM')
+    elif type == 'LEFT':
+        bpy.ops.view3d.view_axis(type='LEFT')
+    elif type == 'RIGHT':
+        bpy.ops.view3d.view_axis(type='RIGHT')
+    else:
+        print(f"Invalid view type: {type}")
+
+def create_2d_animation_workspace():
+    # Create a new 2D Animation workspace
+    workspace_name = '2D Animation'
+    if workspace_name not in bpy.data.workspaces:
+        bpy.data.workspaces.add(name=workspace_name)
+    bpy.context.window.workspace = bpy.data.workspaces[workspace_name]
+
+create_2d_animation_workspace()
+
 switch_to_2d_animation_workspace()
 
 gpencil = create_grease_pencil_object()
 
+
+
 # Ensure we're in draw mode
 bpy.ops.object.mode_set(mode='PAINT_GPENCIL')
+
+
 
 layer = create_grease_pencil_layer(gpencil)
 
 frame = create_grease_pencil_frame(layer)
 
-# # Create a new stroke
-# stroke = frame.strokes.new()
-# stroke.display_mode = '3DSPACE'  # Set the stroke to 3D space
 
-# # Define stroke geometry
-# stroke.points.add(count=2)
-# stroke.points[0].co = (0, 0, 0)
-# stroke.points[1].co = (2, 2, 2)
+switch_view('FRONT')
+
+# Create a new stroke
+stroke = frame.strokes.new()
+stroke.display_mode = '3DSPACE'  # Set the stroke to 3D space
+
+# Define stroke geometry
+stroke.points.add(count=2)
+stroke.points[0].co = (0, 0, 0)
+stroke.points[1].co = (2, 2, 2)
 
 # Return to object mode
 bpy.ops.object.mode_set(mode='OBJECT')
